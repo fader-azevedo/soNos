afc2911fcae7f2db67f323b961b7dfe990be07eb
fader-azevedo <fadermacuvele383@gmail.com>
2017-12-17 17:22:15 +0200

    outro

--- resources/views/admin/adminHome.blade.php
+++ resources/views/admin/adminHome.blade.php
@@ -67,39 +67,51 @@ use App\Turma,App\Inscricao,App\Disciplina;
         </div>
     </div>
 
-    <div class="row">
-        <section class="col-lg-9 connectedSortable">
-            <div class="box box-success">
-                <div class="box-header with-border">
-                    <i class="fa fa-bar-chart-o"></i>
-                    <h3 class="box-title">Pagamento de Mensalidades</h3>
+    <div id="carro" class="carousel slide" data-ride="carousel" data-interval="false">
+        <div  class="carousel-inner" id="divKar">
+            <div class="item active">
+                <div class="row">
+                    <section class="col-lg-9 connectedSortable">
+                        <div class="box box-success">
+                            <div class="box-header with-border">
+                                <i class="fa fa-bar-chart-o"></i>
+                                <h3 class="box-title">Pagamento de Mensalidades</h3>
 
-                </div>
-                <div class="box-body">
-                    <h5 class="centered"><label class="label" style="background-color: #00a65a">Pagas</label>
-                        <label class="label" style="background-color: #f56954">Nao Pagas</label></h5>
-                    <div class="chart" id="bar-chart"  style="height:290px">
-                    </div>
+                            </div>
+                            <div class="box-body">
+                                <h5 class="centered"><label class="label" style="background-color: #00a65a">Pagas</label>
+                                    <label class="label" style="background-color: #f56954">Nao Pagas</label></h5>
+                                <div class="chart" id="bar-chart"  style="height:290px">
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                    <section class="col-lg-3 connectedSortable">
+                        <div class="box box-danger">
+                            <div class="box-header with-border">
+
+                                <i style="color: #00a65a" class="fa fa-money"></i>
+                                <h3 style="color: #00a65a" class="box-title">Pagamento</h3>
+                                <i class="fa fa-times"></i>
+                                <i style="color: red" class="fa fa-money"></i>
+                                <h3 style="color: red" class="box-title">Dividas</h3>
+
+                            </div>
+                            <div class="box-body chart-responsive">
+                                <div class="chart" id="payVSdivida" style="height: 300px;"></div>
+                            </div>
+                        </div>
+                    </section>
                 </div>
             </div>
-        </section>
 
-        <section class="col-lg-3 connectedSortable">
-            <div class="box box-danger">
-                <div class="box-header with-border">
+            <div class="item">
+                <div class="row">
 
-                    <i style="color: #00a65a" class="fa fa-money"></i>
-                    <h3 style="color: #00a65a" class="box-title">Pagamento</h3>
-                    <i class="fa fa-times"></i>
-                    <i style="color: red" class="fa fa-money"></i>
-                    <h3 style="color: red" class="box-title">Dividas</h3>
-
-                </div>
-                <div class="box-body chart-responsive">
-                    <div class="chart" id="payVSdivida" style="height: 300px;"></div>
                 </div>
             </div>
-        </section>
+        </div>
     </div>
 @endsection
 

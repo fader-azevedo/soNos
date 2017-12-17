e520ec8dc6c3e6d8527ae63f5940924c4a964e46
fader-azevedo <fadermacuvele383@gmail.com>
2017-12-17 22:36:51 +0200

    registar

--- resources/views/admin/adminHome.blade.php
+++ resources/views/admin/adminHome.blade.php
@@ -108,7 +108,7 @@ use App\Turma,App\Inscricao,App\Disciplina;
 
             <div class="item">
                 <div class="row">
-
+                    <h1>Canditdatos</h1>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['prefix'=>'mensalidade'], function (){
     Route::get('/','MensalidadeController@index')->middleware(Mdw::class,'auth');
-//    Route::get('registar','MensalidadeController@registarMensalidade');
+    Route::get('registar','MensalidadeController@registarMensalidade')->middleware(Mdw::class,'auth');
 });
 
 /*Rotas de Admin*/

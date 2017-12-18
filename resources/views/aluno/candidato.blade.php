badf2b8b24f0875e4054e1df2b51c981aa803392
fader-azevedo <fadermacuvele383@gmail.com>
2017-12-18 02:34:50 +0200

    dadoos da tabela candidato

--- resources/views/aluno/candidato.blade.php
+++ resources/views/aluno/candidato.blade.php
@@ -25,21 +25,21 @@
                     </tr>
                 </thead>
                 <tbody id="tabela1Corpo">
-                @foreach($candidato  as $ms)
-                    <tr>
-                        <td>
-                            <img src="{{asset('img/alunos/').'/'.$ms->foto}}" class="img-circle " width="50" height="50">
-                        </td>
-                        <td>{{$ms->nome.' '.$ms->apelido}}</td>
-                        <td>{{$ms->sexo}}</td>
-                        <td>{{$ms->numBi}}</td>
-                        <td>{{$ms->dataNasc}}</td>
+                    @foreach($candidato  as $ms)
+                        <tr>
+                            <td>
+                                <img src="{{asset('img/alunos/').'/'.$ms->foto}}" class="img-circle " width="50" height="50">
+                            </td>
+                            <td>{{$ms->nome.' '.$ms->apelido}}</td>
+                            <td>{{$ms->sexo}}</td>
+                            <td>{{$ms->numBi}}</td>
+                            <td>{{$ms->dataNasc}}</td>
 
-                        <td><a class="btn btn-info btn-ver" data-id="{{$ms->id}}"><i class="zmdi zmdi-eye"></i>&nbsp;</a></td>
-                        <td><a class="btn btn-warning"><i class="fa fa-check"></i></a></td>
-                        <td><a class="btn btn-danger"><i class="zmdi zmdi-delete"></i>&nbsp;</a></td>
-                    </tr>
-                @endforeach
+                            <td><a class="btn btn-info btn-ver" data-id="{{$ms->id}}"><i class="zmdi zmdi-eye"></i>&nbsp;</a></td>
+                            <td><a class="btn btn-warning"><i class="fa fa-check"></i></a></td>
+                            <td><a class="btn btn-danger"><i class="zmdi zmdi-delete"></i>&nbsp;</a></td>
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>

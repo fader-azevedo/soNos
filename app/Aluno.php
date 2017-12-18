392cbb91fa2dc169e574c9bc8634191e0cfcc670
fader-azevedo <fadermacuvele383@gmail.com>
2017-12-18 02:35:52 +0200

    construtor de aluno

--- app/Aluno.php
+++ app/Aluno.php
@@ -6,8 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Aluno extends Model{
     protected $fillable =['codigo','apelido','nome','sexo','dataNasc','numBi','naturalidade','residencia','foto','idUser','idContacto','idEncarregado'];
-
-
+    
     public function getInscricao(){
         return $this->hasMany(Inscricao::class,'idAluno');
     }

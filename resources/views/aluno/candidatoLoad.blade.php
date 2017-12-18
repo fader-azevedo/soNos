1cbc27f79014b332e8e719322b81de56dd5a4533
fader-azevedo <fadermacuvele383@gmail.com>
2017-12-18 18:26:50 +0200

    change

--- resources/views/aluno/candidatoLoad.blade.php
+++ resources/views/aluno/candidatoLoad.blade.php
@@ -3,25 +3,30 @@
     <tr>
         <th style="width: 10%">Fotografia</th>
         <th style="width: 30%">Nome Completo</th>
-        <th style="width: 20%">Sexo</th>
+        <th style="width: 15%">Sexo</th>
         <th style="width: 20%">Num BI</th>
         <th style="width: 20%">Data Nasc</th>
+        <th style="width: 20%">Acoes</th>
     </tr>
     </thead>
     <tbody id="tabela1Corpo" class="dados">
         @foreach($candidato  as $ms)
             <tr>
-                <td>
+                <td style="width: 10%">
                     <img src="{{asset('img/alunos/').'/'.$ms->foto}}" class="img-circle " width="50" height="50">
                 </td>
-                <td>{{$ms->nome.' '.$ms->apelido}}</td>
-                <td>{{$ms->sexo}}</td>
-                <td>{{$ms->numBi}}</td>
-                <td>{{$ms->dataNasc}}</td>
+                <td style="width: 30%">{{$ms->nome.' '.$ms->apelido}}</td>
+                <td style="width: 15%">{{$ms->sexo}}</td>
+                <td style="width: 20%">{{$ms->numBi}}</td>
+                <td style="width: 20%">{{$ms->dataNasc}}</td>
 
-                <td><a class="btn btn-info btn-ver" data-id="{{$ms->id}}"><i class="zmdi zmdi-eye"></i>&nbsp;</a></td>
-                <td><a class="btn btn-warning"><i class="fa fa-check"></i></a></td>
-                <td><a class="btn btn-danger"><i class="zmdi zmdi-delete"></i>&nbsp;</a></td>
+                <td style="width: 20%; display: flex">
+                    <a class="btn btn-info btn-ver bt" data-id="{{$ms->id}}"><i class="zmdi zmdi-eye"></i></a>
+                    <a class="btn btn-warning bt"><i class="zmdi zmdi-check"></i></a>
+                    <a class="btn btn-danger bt"><i class="zmdi zmdi-delete"></i></a>
+                </td>
+                {{--<td style="width: 1.5%"></td>--}}
+                {{--<td style="width: 2%"></td>--}}
             </tr>
         @endforeach
     </tbody>

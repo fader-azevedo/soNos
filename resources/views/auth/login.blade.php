f7eb8d2019e3a1953f1bed4cef0cceebe69b3913
fader-azevedo <fadermacuvele383@gmail.com>
2017-12-19 06:50:20 +0200

    remocao de linhas desnecessarias

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,153 +28,85 @@
 </head>
 <body class="hold-transition skin-black sidebar-mini ">
 
-
-<nav class="navbar navbar-default navbar-static-top">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                <span class="sr-only">Toggle Navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="{{ url('/') }}">
-            </a>
-        </div>
-
-        <div class="collapse navbar-collapse" id="app-navbar-collapse">
-            <ul class="nav navbar-nav">&nbsp;
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="{{ route('login') }}">Login</a></li>
-                <li><a href="{{ route('register') }}">Pre-Inscrição</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
-{{--<nav class="navbar navbar-default navbar-static-top">--}}
-    {{--<div class="container">--}}
-{{--<div class="container">--}}
-    {{--<div class="row">--}}
-        {{--<div class="col-md-8 col-md-offset-2">--}}
-            {{--<div class="panel panel-default">--}}
-                {{--<div class="panel-heading">Login</div>--}}
-{{----}}
-                {{--<div class="panel-body">--}}
-                    {{--<form class="form-horizontal" method="POST" action="{{ route('login') }}">--}}
-                        {{--{{ csrf_field() }}--}}
-
-                        {{--<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">--}}
-                            {{--<label for="email" class="col-md-4 control-label">E-Mail Address</label>--}}
-
-                            {{--<div class="col-md-6">--}}
-                                {{--<input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>--}}
-
-                                {{--@if ($errors->has('email'))--}}
-                                    {{--<span class="help-block">--}}
-                                        {{--<strong>{{ $errors->first('email') }}</strong>--}}
-                                    {{--</span>--}}
-                                {{--@endif--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-
-                        {{--<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">--}}
-                            {{--<label for="password" class="col-md-4 control-label">Password</label>--}}
-
-                            {{--<div class="col-md-6">--}}
-                                {{--<input id="password" type="password" class="form-control" name="password" required>--}}
-
-                                {{--@if ($errors->has('password'))--}}
-                                    {{--<span class="help-block">--}}
-                                        {{--<strong>{{ $errors->first('password') }}</strong>--}}
-                                    {{--</span>--}}
-                                {{--@endif--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-
-                        {{--<div class="form-group">--}}
-                            {{--<div class="col-md-6 col-md-offset-4">--}}
-                                {{--<div class="checkbox">--}}
-                                    {{--<label>--}}
-                                        {{--<input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me--}}
-                                    {{--</label>--}}
-                                {{--</div>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-
-                        {{--<div class="form-group">--}}
-                            {{--<div class="col-md-8 col-md-offset-4">--}}
-                                {{--<button type="submit" class="btn btn-primary">--}}
-                                    {{--Login--}}
-                                {{--</button>--}}
-
-                                {{--<a class="btn btn-link" href="{{ route('password.request') }}">--}}
-                                    {{--Forgot Your Password?--}}
-                                {{--</a>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-                    {{--</form>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-
-
-        <form class="form-login"  method="POST" action="{{ route('login') }}">
-            {{ csrf_field() }}
-            <h2 class="form-login-heading">Login</h2>
-            <div class="login-wrap">
-                <div class="input-field form-group{{ $errors->has('email') ? ' has-error' : '' }}" >
-                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-                    <label for="UserName"><i class="zmdi zmdi-account"></i>&nbsp;Email</label>
-
-                    @if ($errors->has('email'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('email') }}</strong>
-                        </span>
-                    @endif
-                </div>
-
-                <div class="input-field form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                    <input id="password" type="password" class="form-control" name="password" required>
-                    <label for="password"><i class="zmdi zmdi-lock"></i>&nbsp;Password</label>
-
-                    @if ($errors->has('password'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('password') }}</strong>
-                        </span>
-                    @endif
-                </div>
-                <label class="checkbox">
-                    <span class="pull-right">
-                        <a data-toggle="modal" href="#myModal"> Esqueceste Password?</a>
-                    </span>
-                </label>
-                <button class="btn btn-theme btn-block" type="submit"><a><i class="zmdi zmdi-lock-open"></i> Login</a></button>
-                <hr>
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="{{ url('/') }}">
+                </a>
             </div>
 
-            <div aria-hidden="true" aria-labelledby="" role="dialog" tabindex="-1" id="myModal" class="modal fade">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                            <h5 class="modal-title">Esqueceste Password ?</h5>
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <ul class="nav navbar-nav">&nbsp;
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="{{ route('login') }}">Login</a></li>
+                    <li><a href="{{ route('register') }}">Pre-Inscrição</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <form class="form-login"  method="POST" action="{{ route('login') }}">
+        {{ csrf_field() }}
+        <h2 class="form-login-heading">Login</h2>
+        <div class="login-wrap">
+            <div class="input-field form-group{{ $errors->has('email') ? ' has-error' : '' }}" >
+                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                <label for="UserName"><i class="zmdi zmdi-account"></i>&nbsp;Email</label>
+
+                @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+            </div>
+
+            <div class="input-field form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                <input id="password" type="password" class="form-control" name="password" required>
+                <label for="password"><i class="zmdi zmdi-lock"></i>&nbsp;Password</label>
+
+                @if ($errors->has('password'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
+            </div>
+            <label class="checkbox">
+                <span class="pull-right">
+                    <a data-toggle="modal" href="#myModal"> Esqueceste Password?</a>
+                </span>
+            </label>
+            <button class="btn btn-theme btn-block" type="submit"><a><i class="zmdi zmdi-lock-open"></i> Login</a></button>
+            <hr>
+        </div>
+
+        <div aria-hidden="true" aria-labelledby="" role="dialog" tabindex="-1" id="myModal" class="modal fade">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                        <h5 class="modal-title">Esqueceste Password ?</h5>
+                    </div>
+                    <div class="modal-body">
+                        <p>Preenche com seu email para restaurar</p>
+                        <div class="input-field">
+                            <input id="email"  type="text" class="validate">
+                            <label for="email" ><i class="zmdi zmdi-email"></i>&nbsp;Email</label>
                         </div>
-                        <div class="modal-body">
-                            <p>Preenche com seu email para restaurar</p>
-                            <div class="input-field">
-                                <input id="email"  type="text" class="validate">
-                                <label for="email" ><i class="zmdi zmdi-email"></i>&nbsp;Email</label>
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <button data-dismiss="modal" class="btn btn-default" type="button">Cancelar</button>
-                            <button class="btn btn-theme" type="button">Enviar</button>
-                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button data-dismiss="modal" class="btn btn-default" type="button">Cancelar</button>
+                        <button class="btn btn-theme" type="button">Enviar</button>
                     </div>
                 </div>
             </div>
-        </form>
+        </div>
+    </form>
 
 <script src="{!! asset('js/jquery.js')!!}"></script>
 <script src="{!! asset('js/bootstrap.min.js')!!}"></script>

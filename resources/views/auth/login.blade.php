5f54df4efbcfc7887fbe90837f4f43d031e94b01
fader-azevedo <fadermacuvele383@gmail.com>
2017-12-19 06:48:12 +0200

    comecando com ajuste de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,9 +12,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-{{--<title>{{ config('app.name', 'Fader') }}</title>--}}
 
-<!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 

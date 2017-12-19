28d58e4b919f0dbbfdae290dcac57cb7858ae4e6
fader-azevedo <fadermacuvele383@gmail.com>
2017-12-19 07:07:31 +0200

    ajuste de %

--- resources/views/aluno/candidatoLoad.blade.php
+++ resources/views/aluno/candidatoLoad.blade.php
@@ -2,11 +2,11 @@
     <thead>
     <tr>
         <th style="width: 8%">Foto</th>
-        <th style="width: 23%">Nome Completo</th>
+        <th style="width: 26%">Nome Completo</th>
         <th style="width: 15%">Sexo</th>
         <th style="width: 18%">Num BI</th>
         <th style="width: 18%">Data Nasc</th>
-        <th style="width: 18%">Opções</th>
+        <th style="width: 15%">Opções</th>
     </tr>
     </thead>
     <tbody id="tabela1Corpo" class="dados">
@@ -15,12 +15,12 @@
                 <td style="width: 8%">
                     <img src="{{asset('img/alunos/').'/'.$ms->foto}}" class="img-circle " width="50" height="50">
                 </td>
-                <td style="width: 23%">{{$ms->nome.' '.$ms->apelido}}</td>
+                <td style="width: 26%">{{$ms->nome.' '.$ms->apelido}}</td>
                 <td style="width: 15%">{{$ms->sexo}}</td>
                 <td style="width: 18%">{{$ms->numBi}}</td>
                 <td style="width: 18%">{{$ms->dataNasc}}</td>
 
-                <td style="width: 18%; display: flex">
+                <td style="width: 15%; display: flex" class="center">
                     <a class="btn btn-info btn-ver bt" data-id="{{$ms->id}}"><i class="zmdi zmdi-eye"></i></a>
                     <a class="btn btn-warning bt"><i class="zmdi zmdi-check"></i></a>
                     <a class="btn btn-danger bt"><i class="zmdi zmdi-delete"></i></a>

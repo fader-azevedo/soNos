1fea4d5977bbe8dac0596751d89b27803bd192e1
fader-azevedo <fadermacuvele383@gmail.com>
2017-12-19 06:49:05 +0200

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,22 +1,11 @@
-{{--@extends('template.index')--}}
-
-{{--@section('filhote')--}}
-
-        <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-
-
     <title>Só Ciência Só Nós</title>
 
     <link rel="stylesheet" type="text/css" href="{!! asset('css/bootstrap.css') !!}"/>
@@ -27,7 +16,6 @@
 
     <link rel="stylesheet" type="text/css" href="{!! asset('css/style.css')!!}" />
     {{--<link rel="stylesheet" type="text/css" href="{!! asset('css/style-responsive.css')!!}"/>--}}
-
 
     <link rel="stylesheet" type="text/css" href="{!! asset('css/_all-skins.css')!!}"/>
     <link rel="stylesheet" type="text/css" href="{!! asset('charts/morris.css')!!}"/>

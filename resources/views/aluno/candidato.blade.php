50bba4b8063cf09ae78380b098a685121d4cac10
fader-azevedo <fadermacuvele383@gmail.com>
2017-12-19 07:15:53 +0200

    butao de disciplinas

--- resources/views/aluno/candidato.blade.php
+++ resources/views/aluno/candidato.blade.php
@@ -59,9 +59,6 @@
                 <div class="box">
                     <div class="box-header with-border text-center">
                         <h3 class="box-title">Disciplinas</h3>
-                        <div class="box-tools pull-right">
-                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-                        </div>
                     </div>
                     <div class="box-body">
                         <ul class="todo-list" id="curso">

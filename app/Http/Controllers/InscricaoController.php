1e56ec7e39842994fddc28406f4c379e83bc2890
fader-azevedo <fadermacuvele383@gmail.com>
2017-12-19 23:02:16 +0200

    eliminando linhas

--- app/Http/Controllers/InscricaoController.php
+++ app/Http/Controllers/InscricaoController.php
@@ -3,13 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Inscricao;
-use Illuminate\Http\Request;
 
 class InscricaoController extends Controller{
     public function getInscricao(){
-        $inscricao = Inscricao::query()
-            ->join('alunos','inscricaos.idAluno','=','alunos.id')
-            ->join('disciplinas','inscricaos.idDisciplina','=','disciplinas.id')
+        $inscricao = Inscricao::query()->join('alunos','inscricaos.idAluno','=','alunos.id')->join('disciplinas','inscricaos.idDisciplina','=','disciplinas.id')
             ->join('contactos','alunos.idContacto','=','contactos.id')
             ->select('disciplinas.*','disciplinas.nome as disciplina','disciplinas.id as idDisciplina','alunos.*','contactos.*')
             ->where('idAluno',$_POST['idAluno'])->where('ano',$_POST['ano'])->get();

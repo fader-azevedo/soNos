519becd7fbfb6c60692e83689bb00a5578a3cd50
fader-azevedo <fadermacuvele383@gmail.com>
2017-12-19 07:05:31 +0200

    mais uma coluna

--- resources/views/aluno/candidatoLoad.blade.php
+++ resources/views/aluno/candidatoLoad.blade.php
@@ -2,8 +2,8 @@
     <thead>
     <tr>
         <th style="width: 8%">Foto</th>
-        <th style="width: 20%">Nome Completo</th>
-        <th style="width: 18%">Sexo</th>
+        <th style="width: 23%">Nome Completo</th>
+        <th style="width: 15%">Sexo</th>
         <th style="width: 18%">Num BI</th>
         <th style="width: 18%">Data Nasc</th>
         <th style="width: 18%">Opções</th>
@@ -12,15 +12,15 @@
     <tbody id="tabela1Corpo" class="dados">
         @foreach($candidato  as $ms)
             <tr>
-                <td style="width: 10%">
+                <td style="width: 8%">
                     <img src="{{asset('img/alunos/').'/'.$ms->foto}}" class="img-circle " width="50" height="50">
                 </td>
-                <td style="width: 30%">{{$ms->nome.' '.$ms->apelido}}</td>
+                <td style="width: 23%">{{$ms->nome.' '.$ms->apelido}}</td>
                 <td style="width: 15%">{{$ms->sexo}}</td>
-                <td style="width: 20%">{{$ms->numBi}}</td>
-                <td style="width: 20%">{{$ms->dataNasc}}</td>
+                <td style="width: 18%">{{$ms->numBi}}</td>
+                <td style="width: 18%">{{$ms->dataNasc}}</td>
 
-                <td style="width: 20%; display: flex">
+                <td style="width: 18%; display: flex">
                     <a class="btn btn-info btn-ver bt" data-id="{{$ms->id}}"><i class="zmdi zmdi-eye"></i></a>
                     <a class="btn btn-warning bt"><i class="zmdi zmdi-check"></i></a>
                     <a class="btn btn-danger bt"><i class="zmdi zmdi-delete"></i></a>

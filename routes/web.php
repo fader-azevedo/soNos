0d976a6c160943b32c1a697060c70d5bb0cd5741
fader-azevedo <fadermacuvele383@gmail.com>
2017-12-20 16:37:04 +0200

    rotas

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,10 @@
 <?php
 use App\Http\Middleware\Mdw;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
-
 //Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
@@ -30,5 +17,4 @@ Route::group(['prefix'=>'mensalidade'], function (){
 
 Route::get('/candidato','AlunoController@candidato')->name('candidato')->middleware(Mdw::class,'auth');
 //Route::get('usuarios',['as'=>'users.index','uses'=>'AlunoController@index']);
-
 Route::resource('aluno','AlunoController');

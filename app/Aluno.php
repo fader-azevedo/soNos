6b61b480c8095fa2d8387b1e13d508236c8f3c89
fader-azevedo <fadermacuvele383@gmail.com>
2018-01-15 11:20:24 +0200

    commit

--- app/Aluno.php
+++ app/Aluno.php
@@ -10,10 +10,14 @@ use Sortable;
 
     protected $fillable =['codigo','apelido','nome','sexo','dataNasc','numBi','naturalidade','residencia','foto','idUser','idContacto','idEncarregado'];
 
-    
+
 //    protected $sortable = ['apelido','nome','sexo'];
 
+//    public function getInscricao(){
+//        return $this->hasMany(Inscricao::class,'idAluno');
+//    }
+
     public function getInscricao(){
-        return $this->hasMany(Inscricao::class,'idAluno');
+        return $this->morphToMany(Inscricao::class,'inscricao');
     }
 }

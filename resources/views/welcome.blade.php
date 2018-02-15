4c0a3b68a130a9f3fd03fe21a91c32fa5608d9dc
fader-azevedo <fadermacuvele383@gmail.com>
2018-02-15 17:10:25 +0200

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -76,7 +76,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Pagina Web
+                    Pagina Web Ainda por vir
                 </div>
             </div>
         </div>
